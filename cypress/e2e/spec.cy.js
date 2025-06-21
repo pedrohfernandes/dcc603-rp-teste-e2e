9477dfa0e6bec6f4b03fc770c59581e7474c1648
Pedro Henrique <pedrohfernandes2001@gmail.com>
2025-06-21 17:43:29 -0300

    Criação de teste E2E

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,95 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Insere e edita uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+
+    cy.get('[data-cy=todos-list]')
+      .dblclick()
+      .find('li.editing > input.edit')
+      .clear()
+      .type('Tarefa editada{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Tarefa editada');
+  });
+
+  it('Marca e desmarca uma tarefa, verificando os filtros', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}');
+
+    //filtra por ativo
+    cy.get('[data-cy=filter-active-link')
+      .click();
+      
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    //marca como completado
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    //filtra por completado
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    //desmarca
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    //filtra por ativo de novo
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+  });
+
+  it('Limpa tarefas completadas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .last()
+      .click(); 
+
+    cy.get('[data-cy=clear-completed-button]')
+      .click();
+    cy.get('[data-cy=todos-list]') 
+      .children()
+      .should('have.length', 0);
+  });
 });
